faab1dd023be323c568df0e5264a3c994ac67615
Hirotoki Sato <tokihiro.k@gmail.com>
2016-03-14 23:33:36 +0900

    puyo

---
+++ GameController.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class GameController : MonoBehaviour {
+	public int horizon, vertical, createPosX, createPosY;
+	public GameObject puyo;
+	GameObject activePuyo;
+	puyo activePuyoScript;
+	GameObject[,] objMap;
+	int[,] map;
+	List<float> pointXList, pointYList;
+	Coroutine co;
+
+	enum State {
+		Ready,
+		Play,
+		GameOver
+	}
+
+	State state;
+
+
+	void Start () {
+		state = State.Ready;
+	}
+
+	void GameReady () {
+		objMap = new GameObject[horizon, vertical];
+		map = new int[horizon + 2, vertical + 2];
+		pointXList = new List<float> ();
+		pointYList = new List<float> ();
+
+		for (int i = 0; i < horizon + 2; i++) {
+			for (int j = 0; j < vertical + 2; j++) {
+				if (i == 0 || i == horizon + 1 || j == 0) {
+					map [i, j] = 10;
+				} else {
+					map [i, j] = 0;
+				}
+			}
+		}
+
+		for (int i = 0; i < horizon + 2; i++) {
+			pointXList.Add (-2.5f + i * 0.7f);
+		}
+
+		for (int i = 0; i < vertical + 2; i++) {
+			pointYList.Add (-4.2f + i * 0.45f);
+		}
+	}
+
+	void CreatePuyo () {
+		activePuyo = (GameObject) Instantiate (puyo, this.transform.position, Quaternion.identity);
+		activePuyoScript = activePuyo.GetComponentInChildren<puyo> ();
+		activePuyoScript.setDefaultPos (ref pointXList, ref pointYList, createPosX, createPosY);
+		if (co != null) {
+			StopCoroutine (co);
+		}
+		co = StartCoroutine ("autoDown");
+	}
+
+	private IEnumerator autoDown() {
+		while (true) {
+			yield return new WaitForSeconds (1.0f);
+			if (!activePuyoScript.moveDown (ref pointXList, ref pointYList, ref map)) {
+				nextTurn ();
+			}
+		}
+	}
+
+	private void nextTurn() {
+		CreatePuyo ();
+	}
+
+	void Update () {
+		switch(state) {
+		case State.Ready:
+			if (Input.GetKeyDown ("down")) {
+				GameReady ();
+				CreatePuyo ();
+				state = State.Play;
+			}
+			break;
+		case State.Play:
+			if (Input.GetKeyDown ("down")) {
+				if (!activePuyoScript.moveDown (ref pointXList, ref pointYList, ref map)) {
+					nextTurn ();
+				}
+			}
+			if (Input.GetKeyDown ("left")) {
+				activePuyoScript.moveLeft (ref pointXList, ref pointYList, ref map);
+			}
+			if (Input.GetKeyDown ("right")) {
+				activePuyoScript.moveRight (ref pointXList, ref pointYList, ref map);
+			}
+			break;
+		case State.GameOver:
+			break;
+		default:
+			break;
+		}
+	}
+}
---
+++ puyo.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class puyo : MonoBehaviour {
+	public GameObject[] puyopuyo;
+	GameObject[] puyoList;
+	int[,] puyoIndex;
+
+	// Use this for initialization
+	void Awake () {
+		puyoList = new GameObject[2];
+		puyoIndex = new int[2, 2];
+
+		int p1 = Random.Range (0, 4);
+		int p2 = Random.Range (0, 4);
+
+		puyoList[0] = (GameObject) Instantiate (puyopuyo[p1], this.transform.position, Quaternion.identity);
+		puyoList[1] = (GameObject) Instantiate (puyopuyo[p2], this.transform.position, Quaternion.identity);
+		puyoList [0].SetActive (false);
+		puyoList [1].SetActive (false);
+	}
+
+	public void setDefaultPos(ref List<float> pointXList, ref List<float> pointYList, int indexX, int indexY) {
+		puyoList [0].transform.position = new Vector3 (pointXList[indexX], pointYList[indexY]);
+		puyoList [1].transform.position = new Vector3 (pointXList[indexX], pointYList[indexY - 1]);
+		puyoIndex [0, 0] = indexX; puyoIndex [0, 1] = indexY;
+		puyoIndex [1, 0] = indexX; puyoIndex [1, 1] = indexY - 1;
+		puyoList [0].SetActive (true);
+		puyoList [1].SetActive (true);
+	}
+
+	public bool moveDown (ref List<float> pointXList, ref List<float> pointYList, ref int [,] map) {
+		if (!isPointEnable(puyoIndex[0, 0], puyoIndex[0, 1] - 1, ref map) || !isPointEnable(puyoIndex[1, 0], puyoIndex[1, 1] - 1, ref map)) {
+			map[puyoIndex[0, 0], puyoIndex[0, 1]] = 1;
+			map[puyoIndex[1, 0], puyoIndex[1, 1]] = 1;
+			return false;
+		}
+
+		puyoIndex [0, 1] = puyoIndex[0, 1] - 1;
+		puyoIndex [1, 1] = puyoIndex[1, 1] - 1;
+		puyoList [0].transform.position = new Vector3 (pointXList[puyoIndex [0, 0]], pointYList[puyoIndex [0, 1]]);
+		puyoList [1].transform.position = new Vector3 (pointXList[puyoIndex [1, 0]], pointYList[puyoIndex [1, 1]]);
+
+		return true;
+	}
+
+	public void moveLeft (ref List<float> pointXList, ref List<float> pointYList, ref int [,] map) {
+		if (isPointEnable(puyoIndex[0, 0] - 1, puyoIndex[0, 1], ref map) && isPointEnable(puyoIndex[1, 0] - 1, puyoIndex[1, 1], ref map)) {
+			puyoIndex [0, 0] = puyoIndex[0, 0] - 1;
+			puyoIndex [1, 0] = puyoIndex[1, 0] - 1;
+			puyoList [0].transform.position = new Vector3 (pointXList[puyoIndex [0, 0]], pointYList[puyoIndex [0, 1]]);
+			puyoList [1].transform.position = new Vector3 (pointXList[puyoIndex [1, 0]], pointYList[puyoIndex [1, 1]]);
+		}
+	}
+
+	public void moveRight (ref List<float> pointXList, ref List<float> pointYList, ref int [,] map) {
+		if (isPointEnable(puyoIndex[0, 0] + 1, puyoIndex[0, 1], ref map) && isPointEnable(puyoIndex[1, 0] + 1, puyoIndex[1, 1], ref map)) {
+			puyoIndex [0, 0] = puyoIndex[0, 0] + 1;
+			puyoIndex [1, 0] = puyoIndex[1, 0] + 1;
+			puyoList [0].transform.position = new Vector3 (pointXList[puyoIndex [0, 0]], pointYList[puyoIndex [0, 1]]);
+			puyoList [1].transform.position = new Vector3 (pointXList[puyoIndex [1, 0]], pointYList[puyoIndex [1, 1]]);
+		}
+	}
+
+//	public void rotate_r () {
+//		if (isPointEnable(puyoIndex[0, 0] + 1, puyoIndex[0, 1], ref map) && isPointEnable(puyoIndex[1, 0] + 1, puyoIndex[1, 1], ref map)) {
+//			puyoIndex [0, 0] = puyoIndex[0, 0] + 1;
+//			puyoIndex [1, 0] = puyoIndex[1, 0] + 1;
+//			puyoList [0].transform.position = new Vector3 (pointXList[puyoIndex [0, 0]], pointYList[puyoIndex [0, 1]]);
+//			puyoList [1].transform.position = new Vector3 (pointXList[puyoIndex [1, 0]], pointYList[puyoIndex [1, 1]]);
+//		}
+//	}
+
+	private bool isPointEnable(int indexX, int indexY, ref int[,] map) {
+		if (map [indexX, indexY] != 0) {
+			return false;
+		}
+		return true;
+	}
+}
